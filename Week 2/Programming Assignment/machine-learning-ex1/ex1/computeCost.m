aae6af9f244d794ef7b09e1b15177a617c957a42
Vishal Kumar <43850961+trueliar08@users.noreply.github.com>
2020-10-10 17:07:02 +0530

    Updated computecost.m

--- Week 2/Programming Assignment/machine-learning-ex1/ex1/computeCost.m
+++ Week 2/Programming Assignment/machine-learning-ex1/ex1/computeCost.m
@@ -1,11 +1,21 @@
 function J = computeCost(X, y, theta)
-% J = COMPUTECOST(X, y, theta) computes the cost for linear regression 
-% using theta as the parameter for linear regression to fit the data 
-% points in X and y
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
 
-m = length(y);
+% Initialize some useful values
 
+% You need to return the following variables correctly 
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+
+m=length(y);
 i = 1:m;
-J = (1/(2*m)) * sum( ((theta(1) + theta(2) .* X(i,2)) - y(i)) .^ 2); % Un-Vectorized
+J = (1/(2*m)) * sum( ((theta(1) + theta(2) .* X(i,2)) - y(i)) .^ 2);
+
+
+% =========================================================================
 
 end
